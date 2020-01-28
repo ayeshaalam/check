ee49a0ea4e242ebc8e25eb78c5591f14d1828989
ayeshaalam <s.ayeshaalam@gmail.com>
2020-01-29 01:47:24 +0500

    initial commit

---
+++ src/main/kotlin/pk/edu/neduet/cct/elephant/application2/Application2Application.kt
@@ -0,0 +1,11 @@
+package pk.edu.neduet.cct.elephant.application2
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Application2Application
+
+fun main(args: Array<String>) {
+    runApplication<Application2Application>(*args)
+}
